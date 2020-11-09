b80bec902bf9825699abbe001dd8afe2ce8ccce9
Nico <nico.bakomihalis@ierustech.com>
2020-11-09 15:11:14 -0600

    specified kernel name

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,3 +51,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# nbsphinx configurations
+nbsphinx_kernel_name = "nicobako.github.io"
